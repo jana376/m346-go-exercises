d8d9e69af094e49b335968c4967a7a02bac5fbf6
JANA SLAVNIC <jana@slavnic.com>
2025-11-21 14:40:28 +0100

    Aufgabe 1 gelöst

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Jana"
+	var lastName string = "Slavnic"
+	var dayOfBirth int = 23
+	var monthOfBirth int = 11
+	var yearOfBirth int = 2008
+	var numberOfSiblings int = 0
+	const heightInMeters float32 = 1.72
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
